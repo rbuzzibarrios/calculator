5617354d3d392eb235f4f2a76f8d0dfaed7fe5a9
Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>
2020-10-20 22:22:03 -0400

    Initialize Calculator class (#1)
    * initialize calculator class
    
    * create attributes numbers, and  create getters and setters functions

---
+++ application/models/Calculator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Application\models;
+
+class Calculator
+{
+    /** @var int */
+    protected $firstNumber;
+
+    /** @var int */
+    protected $secondNumber;
+
+    /**
+     * Calculator constructor.
+     * @param $firstNumber
+     * @param $secondNumber
+     */
+    public function __construct($firstNumber, $secondNumber)
+    {
+        $this->firstNumber = $firstNumber;
+        $this->secondNumber = $secondNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstNumber()
+    {
+        return $this->firstNumber;
+    }
+
+    /**
+     * @param mixed $firstNumber
+     */
+    public function setFirstNumber($firstNumber)
+    {
+        $this->firstNumber = $firstNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecondNumber()
+    {
+        return $this->secondNumber;
+    }
+
+    /**
+     * @param mixed $secondNumber
+     */
+    public function setSecondNumber($secondNumber)
+    {
+        $this->secondNumber = $secondNumber;
+    }
+}
