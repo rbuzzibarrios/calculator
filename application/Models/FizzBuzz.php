07ee2b92e266b99fab221ef2f726725c0f0b84bf
jedv3588 <jedv3588@gmail.com>
2020-10-28 22:38:39 -0500

    Adding implementation of run function in FizzBuzz class

--- application/Models/FizzBuzz.php
+++ application/Models/FizzBuzz.php
@@ -69,6 +69,19 @@ class FizzBuzz{
         return $output;
     }
 
+    /**
+     * function run.
+     * return a list with results of is_fizz_buzz function
+     * $max_num is the length of the array  
+     */
+    public function run(){
+
+        for($i = 1; $i <= $max_num; $i++){
+            echo $i." ".fizz_buzz($i);
+        }
+
+    }
+
 }
 
 ?>
