068b380023dc6d44b2e14418aa00d015299606b5
jedv3588 <jedv3588@gmail.com>
2020-10-28 22:36:26 -0500

    Adding implementation of is_fizz_buzz function

--- application/Models/FizzBuzz.php
+++ application/Models/FizzBuzz.php
@@ -36,6 +36,39 @@ class FizzBuzz{
         $this->max_number = $max_number;
     }
 
+    /**
+     * function is_fizz_buzz.
+     * return Fizz if $n is multiple of 3
+     * return Buzz if $n is multiple of 5
+     * return FizzBuzz if $n is multiple of both 3 and 5
+     * return the number if $n isen't a multiple of 3 or 5 
+     */
+    function is_fizz_buzz($n){
+
+        $output = '';
+        
+            switch(true){
+
+                case($n % 15 === 0):
+                    $output .= "FizzBuzz"."\n";
+                    break;
+        
+                case($n % 3 === 0 ):
+                    $output .= "Fizz"."\n";
+                    break;
+        
+                case($n % 5 === 0 ):
+                    $output .= "Buzz"."\n";
+                    break;
+        
+                default:
+                    $output .= $n."\n";
+                    break;
+            }
+        
+        return $output;
+    }
+
 }
 
 ?>
