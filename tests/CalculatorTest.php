881cc595f019706ea16f472a9c8dd491aa6da54b
Julio Enrique Duque Vizcaino <68433228+jedv3588@users.noreply.github.com>
2020-11-05 15:09:18 -0600

    Adicionando clase CalculatorTest con primer test a funcion sum (#14)
    * Update CalculatorTest.php
    
    * Adicionando clase CalculatorTest con primer test a funcion sum
    
    * Adicion test suma de flotantes
    
    * Update tests/CalculatorTest.php
    
    Co-authored-by: Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>
    
    * Adicion phpunit.xml
    
    * Update composer.json
    
    * fix conflictin composer.json
    
    * fix no_spaces_inside_parenthesi
    
    Co-authored-by: Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Tests;
+
+use Application\Models\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * Test para comprobar la funcionalidad del metodo sum con enteros
+     */
+    public function testSum()
+    {
+        $calculator = new Calculator(1, 2);
+        $this->assertEquals(3, $calculator->sum(1, 2), 'El resultado de 1 + 2 debe ser 3.');
+    }
+
+    /**
+     * Test para comprobar la funcionalidad del metodo sum con flotantes
+     */
+    public function testSumFloat()
+    {
+        $calculator = new Calculator(1.5, 2.4);
+        $this->assertEquals(3.9, $calculator->sum(1.5, 2.4), 'El resultado de 1.5 + 2.4 debe ser 3.9');
+    }
+}
