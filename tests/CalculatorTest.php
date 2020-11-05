10cd45b14a3811473ff5acbcc4854e306ba39a1e
jedv3588 <jedv3588@gmail.com>
2020-11-05 16:50:47 -0600

    Adicion de tests para laa funcion subtraction

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -24,4 +24,22 @@ class CalculatorTest extends TestCase
         $calculator = new Calculator(1.5, 2.4);
         $this->assertEquals(3.9, $calculator->sum(1.5, 2.4), 'El resultado de 1.5 + 2.4 debe ser 3.9');
     }
+
+    /**
+     * Test para comprobar la funcionalidad del metodo subtraction con enteros
+     */
+    public function testSubtraction()
+    {
+        $calculator = new Calculator(8, 3);
+        $this->assertEquals(5, $calculator->subtraction(8, 3), 'El resultado de 8 - 3 debe ser 5.');
+    }
+
+    /**
+     * Test para comprobar la funcionalidad del metodo subtraction con flotantes
+     */
+    public function testSubtractionFloat()
+    {
+        $calculator = new Calculator(5.8, 3.7);
+        $this->assertEquals(2.1, $calculator->subtraction(5.8, 3.7), 'El resultado de 5.8 - 3.7 debe ser 2.1.');
+    }
 }
