3257b2949c211c73447c16638d51421e35aafae1
Julio Enrique Duque Vizcaino <jduque@cigb.edu.cu>
2020-11-10 12:27:48 -0500

    Declaracion FizzBuzzTest class

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Application\Tests;
+
+use Application\Models\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+
+    /**
+     * Test para comprobar la funcinalidad del metodo isFizzBuzz
+     */
+    public function testIsFizzBuzz()
+    {
+        
+    }
+
+}
+
+?>
